30554920178f9c5bae37bc37994fa70be8b2a436
Meeko11 <162523217+Meeko11@users.noreply.github.com>
2024-09-03 16:09:25 +0800

    Add files via upload

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.clearall = New System.Windows.Forms.Button()
+        Me.modolus = New System.Windows.Forms.Button()
+        Me.deivde = New System.Windows.Forms.Button()
+        Me.seven = New System.Windows.Forms.Button()
+        Me.four = New System.Windows.Forms.Button()
+        Me.zero = New System.Windows.Forms.Button()
+        Me.five = New System.Windows.Forms.Button()
+        Me.eight = New System.Windows.Forms.Button()
+        Me.dot = New System.Windows.Forms.Button()
+        Me.six = New System.Windows.Forms.Button()
+        Me.nine = New System.Windows.Forms.Button()
+        Me.equals = New System.Windows.Forms.Button()
+        Me.minus = New System.Windows.Forms.Button()
+        Me.times = New System.Windows.Forms.Button()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.pluss = New System.Windows.Forms.Button()
+        Me.three = New System.Windows.Forms.Button()
+        Me.two = New System.Windows.Forms.Button()
+        Me.one = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'clearall
+        '
+        Me.clearall.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.clearall.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearall.ForeColor = System.Drawing.Color.Red
+        Me.clearall.Location = New System.Drawing.Point(25, 164)
+        Me.clearall.Name = "clearall"
+        Me.clearall.Size = New System.Drawing.Size(83, 85)
+        Me.clearall.TabIndex = 1
+        Me.clearall.Text = "C/A"
+        Me.clearall.UseVisualStyleBackColor = False
+        '
+        'modolus
+        '
+        Me.modolus.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.modolus.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.modolus.ForeColor = System.Drawing.Color.White
+        Me.modolus.Location = New System.Drawing.Point(124, 164)
+        Me.modolus.Name = "modolus"
+        Me.modolus.Size = New System.Drawing.Size(83, 85)
+        Me.modolus.TabIndex = 3
+        Me.modolus.Text = "%"
+        Me.modolus.UseVisualStyleBackColor = False
+        '
+        'deivde
+        '
+        Me.deivde.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.deivde.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.deivde.ForeColor = System.Drawing.Color.Cyan
+        Me.deivde.Location = New System.Drawing.Point(327, 164)
+        Me.deivde.Name = "deivde"
+        Me.deivde.Size = New System.Drawing.Size(83, 85)
+        Me.deivde.TabIndex = 4
+        Me.deivde.Text = "÷"
+        Me.deivde.UseVisualStyleBackColor = False
+        '
+        'seven
+        '
+        Me.seven.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.seven.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.seven.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.seven.Location = New System.Drawing.Point(25, 265)
+        Me.seven.Name = "seven"
+        Me.seven.Size = New System.Drawing.Size(83, 85)
+        Me.seven.TabIndex = 6
+        Me.seven.Text = "7"
+        Me.seven.UseVisualStyleBackColor = False
+        '
+        'four
+        '
+        Me.four.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.four.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.four.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.four.Location = New System.Drawing.Point(25, 365)
+        Me.four.Name = "four"
+        Me.four.Size = New System.Drawing.Size(83, 85)
+        Me.four.TabIndex = 7
+        Me.four.Text = "4"
+        Me.four.UseVisualStyleBackColor = False
+        '
+        'zero
+        '
+        Me.zero.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.zero.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.zero.ForeColor = System.Drawing.Color.White
+        Me.zero.Location = New System.Drawing.Point(25, 569)
+        Me.zero.Name = "zero"
+        Me.zero.Size = New System.Drawing.Size(182, 85)
+        Me.zero.TabIndex = 11
+        Me.zero.Text = "0"
+        Me.zero.UseVisualStyleBackColor = False
+        '
+        'five
+        '
+        Me.five.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.five.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.five.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.five.Location = New System.Drawing.Point(124, 365)
+        Me.five.Name = "five"
+        Me.five.Size = New System.Drawing.Size(83, 85)
+        Me.five.TabIndex = 10
+        Me.five.Text = "5"
+        Me.five.UseVisualStyleBackColor = False
+        '
+        'eight
+        '
+        Me.eight.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.eight.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.eight.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.eight.Location = New System.Drawing.Point(124, 265)
+        Me.eight.Name = "eight"
+        Me.eight.Size = New System.Drawing.Size(83, 85)
+        Me.eight.TabIndex = 9
+        Me.eight.Text = "8"
+        Me.eight.UseVisualStyleBackColor = False
+        '
+        'dot
+        '
+        Me.dot.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.dot.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dot.ForeColor = System.Drawing.Color.White
+        Me.dot.Location = New System.Drawing.Point(225, 569)
+        Me.dot.Name = "dot"
+        Me.dot.Size = New System.Drawing.Size(83, 85)
+        Me.dot.TabIndex = 14
+        Me.dot.Text = "."
+        Me.dot.UseVisualStyleBackColor = False
+        '
+        'six
+        '
+        Me.six.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.six.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.six.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.six.Location = New System.Drawing.Point(225, 365)
+        Me.six.Name = "six"
+        Me.six.Size = New System.Drawing.Size(83, 85)
+        Me.six.TabIndex = 13
+        Me.six.Text = "6"
+        Me.six.UseVisualStyleBackColor = False
+        '
+        'nine
+        '
+        Me.nine.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.nine.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.nine.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.nine.Location = New System.Drawing.Point(225, 265)
+        Me.nine.Name = "nine"
+        Me.nine.Size = New System.Drawing.Size(83, 85)
+        Me.nine.TabIndex = 12
+        Me.nine.Text = "9"
+        Me.nine.UseVisualStyleBackColor = False
+        '
+        'equals
+        '
+        Me.equals.BackColor = System.Drawing.Color.FromArgb(CType(CType(84, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(227, Byte), Integer))
+        Me.equals.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.equals.ForeColor = System.Drawing.Color.White
+        Me.equals.Location = New System.Drawing.Point(327, 569)
+        Me.equals.Name = "equals"
+        Me.equals.Size = New System.Drawing.Size(83, 85)
+        Me.equals.TabIndex = 17
+        Me.equals.Text = "="
+        Me.equals.UseVisualStyleBackColor = False
+        '
+        'minus
+        '
+        Me.minus.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.minus.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minus.ForeColor = System.Drawing.Color.Cyan
+        Me.minus.Location = New System.Drawing.Point(327, 365)
+        Me.minus.Name = "minus"
+        Me.minus.Size = New System.Drawing.Size(83, 85)
+        Me.minus.TabIndex = 16
+        Me.minus.Text = "-"
+        Me.minus.UseVisualStyleBackColor = False
+        '
+        'times
+        '
+        Me.times.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.times.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.times.ForeColor = System.Drawing.Color.Cyan
+        Me.times.Location = New System.Drawing.Point(327, 265)
+        Me.times.Name = "times"
+        Me.times.Size = New System.Drawing.Size(83, 85)
+        Me.times.TabIndex = 15
+        Me.times.Text = "x"
+        Me.times.UseVisualStyleBackColor = False
+        '
+        'clear
+        '
+        Me.clear.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clear.ForeColor = System.Drawing.Color.Red
+        Me.clear.Location = New System.Drawing.Point(225, 164)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(83, 85)
+        Me.clear.TabIndex = 18
+        Me.clear.Text = "C"
+        Me.clear.UseVisualStyleBackColor = False
+        '
+        'pluss
+        '
+        Me.pluss.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.pluss.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pluss.ForeColor = System.Drawing.Color.Cyan
+        Me.pluss.Location = New System.Drawing.Point(327, 465)
+        Me.pluss.Name = "pluss"
+        Me.pluss.Size = New System.Drawing.Size(83, 85)
+        Me.pluss.TabIndex = 19
+        Me.pluss.Text = "+"
+        Me.pluss.UseVisualStyleBackColor = False
+        '
+        'three
+        '
+        Me.three.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.three.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.three.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.three.Location = New System.Drawing.Point(225, 465)
+        Me.three.Name = "three"
+        Me.three.Size = New System.Drawing.Size(83, 85)
+        Me.three.TabIndex = 20
+        Me.three.Text = "3"
+        Me.three.UseVisualStyleBackColor = False
+        '
+        'two
+        '
+        Me.two.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.two.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.two.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.two.Location = New System.Drawing.Point(124, 465)
+        Me.two.Name = "two"
+        Me.two.Size = New System.Drawing.Size(83, 85)
+        Me.two.TabIndex = 21
+        Me.two.Text = "2"
+        Me.two.UseVisualStyleBackColor = False
+        '
+        'one
+        '
+        Me.one.BackColor = System.Drawing.Color.FromArgb(CType(CType(148, Byte), Integer), CType(CType(146, Byte), Integer), CType(CType(145, Byte), Integer))
+        Me.one.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!)
+        Me.one.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.one.Location = New System.Drawing.Point(25, 465)
+        Me.one.Name = "one"
+        Me.one.Size = New System.Drawing.Size(83, 85)
+        Me.one.TabIndex = 22
+        Me.one.Text = "1"
+        Me.one.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AcceptsReturn = True
+        Me.TextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Window
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(25, 28)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(385, 114)
+        Me.TextBox1.TabIndex = 24
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(443, 706)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.one)
+        Me.Controls.Add(Me.two)
+        Me.Controls.Add(Me.three)
+        Me.Controls.Add(Me.pluss)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.equals)
+        Me.Controls.Add(Me.minus)
+        Me.Controls.Add(Me.times)
+        Me.Controls.Add(Me.dot)
+        Me.Controls.Add(Me.six)
+        Me.Controls.Add(Me.nine)
+        Me.Controls.Add(Me.zero)
+        Me.Controls.Add(Me.five)
+        Me.Controls.Add(Me.eight)
+        Me.Controls.Add(Me.four)
+        Me.Controls.Add(Me.seven)
+        Me.Controls.Add(Me.deivde)
+        Me.Controls.Add(Me.modolus)
+        Me.Controls.Add(Me.clearall)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents clearall As Button
+    Friend WithEvents modolus As Button
+    Friend WithEvents deivde As Button
+    Friend WithEvents seven As Button
+    Friend WithEvents four As Button
+    Friend WithEvents zero As Button
+    Friend WithEvents five As Button
+    Friend WithEvents eight As Button
+    Friend WithEvents dot As Button
+    Friend WithEvents six As Button
+    Friend WithEvents nine As Button
+    Friend WithEvents equals As Button
+    Friend WithEvents minus As Button
+    Friend WithEvents times As Button
+    Friend WithEvents clear As Button
+    Friend WithEvents pluss As Button
+    Friend WithEvents three As Button
+    Friend WithEvents two As Button
+    Friend WithEvents one As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,162 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
+
+Public Class Form1
+    Dim num1 As Decimal
+    Dim num2 As Decimal
+    Dim Operations As Integer
+
+
+    Dim Operator_Selector As Boolean = False
+
+    Private Sub Button4_Click(ByVal sender As Object, e As EventArgs) Handles one.Click
+        Dim v = TextBox1.Text <> "1"
+        TextBox1.Text += "1"
+
+
+    End Sub
+
+    Private Sub tow_Click(ByVal sender As Object, e As EventArgs) Handles two.Click
+        Dim v = TextBox1.Text <> "2"
+        TextBox1.Text += "2"
+
+    End Sub
+
+
+    Private Sub three_Click(ByVal sender As Object, e As EventArgs) Handles three.Click
+        Dim v = TextBox1.Text <> "3"
+        TextBox1.Text += "3"
+
+    End Sub
+
+    Private Sub four_Click(ByVal sender As Object, e As EventArgs) Handles four.Click
+        Dim v = TextBox1.Text <> "4"
+        TextBox1.Text += "4"
+
+    End Sub
+
+    Private Sub five_Click(ByVal sender As Object, e As EventArgs) Handles five.Click
+        Dim v = TextBox1.Text <> "5"
+        TextBox1.Text += "5"
+
+    End Sub
+
+    Private Sub six_Click(ByVal sender As Object, e As EventArgs) Handles six.Click
+        Dim v = TextBox1.Text <> "5"
+        TextBox1.Text += "6"
+
+    End Sub
+
+    Private Sub seven_Click(ByVal sender As Object, e As EventArgs) Handles seven.Click
+        Dim v = TextBox1.Text <> "7"
+        TextBox1.Text += "7"
+
+    End Sub
+
+    Private Sub eight_Click(ByVal sender As Object, e As EventArgs) Handles eight.Click
+        Dim v = TextBox1.Text <> "8"
+        TextBox1.Text += "8"
+    End Sub
+
+    Private Sub nine_Click(sender As Object, e As EventArgs) Handles nine.Click
+        Dim v = TextBox1.Text <> "9"
+        TextBox1.Text += "9"
+
+
+    End Sub
+
+    Private Sub zero_Click(ByVal sender As Object, e As EventArgs) Handles zero.Click
+        Dim v = TextBox1.Text <> "0"
+        TextBox1.Text = "0"
+
+
+    End Sub
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles clear.Click
+        ' TextBox1.Text = ""
+        If TextBox1.Text.Length > 0 Then
+
+        End If
+        TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1)
+
+    End Sub
+
+    Private Sub dot_Click(ByVal sender As Object, e As EventArgs) Handles dot.Click
+        If Not (TextBox1.Text.Contains(".")) Then
+            TextBox1.Text += "."
+        End If
+    End Sub
+
+    Private Sub deivde_Click(ByVal sender As Object, e As EventArgs) Handles deivde.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 4 ' = / 
+    End Sub
+
+    Private Sub pluss_Click(ByVal sender As Object, e As EventArgs) Handles pluss.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 1 ' = +
+
+
+    End Sub
+
+    Private Sub minus_Click(ByVal sender As Object, e As EventArgs) Handles minus.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 2 ' = -
+    End Sub
+
+    Private Sub times_Click(ByVal sender As Object, e As EventArgs) Handles times.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 3 ' = *
+    End Sub
+
+    Private Sub equals_Click(ByVal sender As Object, e As EventArgs) Handles equals.Click
+        If Operator_Selector = True Then
+
+
+
+            num2 = TextBox1.Text
+            If Operations = 1 Then
+                TextBox1.Text = num1 + num2
+
+            ElseIf Operations = 2 Then
+                TextBox1.Text = num1 - num2
+            ElseIf Operations = 3 Then
+                TextBox1.Text = num1 * num2
+            ElseIf Operations = 4 Then
+                TextBox1.Text = num1 * num2 / 100
+            Else
+                If num2 = 0 Then
+                    TextBox1.Text = "ERROR!"
+                Else
+                    TextBox1.Text = num1 / num2
+                End If
+
+            End If
+            Operator_Selector = False
+
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub modolus_Click(sender As Object, e As EventArgs) Handles modolus.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = "0"
+        Operator_Selector = True
+        Operations = 4 '% = *
+    End Sub
+
+    Private Sub clearall_Click(sender As Object, e As EventArgs) Handles clearall.Click
+        TextBox1.Text = ""
+    End Sub
+End Class
