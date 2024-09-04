7fba617405225b0fcaf3b6eec21a2101036bec11
Meeko11 <162523217+Meeko11@users.noreply.github.com>
2024-09-04 19:42:40 +0800

    Add files via upload

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.clearall = New System.Windows.Forms.Button()
         Me.modolus = New System.Windows.Forms.Button()
         Me.deivde = New System.Windows.Forms.Button()
@@ -312,8 +313,9 @@ Partial Class Form1
         Me.Controls.Add(Me.deivde)
         Me.Controls.Add(Me.modolus)
         Me.Controls.Add(Me.clearall)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "calculator"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
